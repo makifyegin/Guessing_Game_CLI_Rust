f9deb837ea55646e5a6be91427ff59274ca593ed
Mehmet Akif Yegin <94207328+makifyegin@users.noreply.github.com>
2024-05-19 21:24:44 +0000

    dev

--- guessing_game/src/lib.rs
+++ guessing_game/src/lib.rs
@@ -1,6 +1,3 @@
-
-
-
 // Create a function create random number
 pub fn create_random_number() -> u32 {
     use rand::Rng;
@@ -11,17 +8,14 @@ pub fn create_random_number() -> u32 {
 // Create a function to get number from user
 pub fn get_number_from_user() -> u32 {
     let mut guess = String::new();
-    std::io::stdin().read_line(&mut guess).expect("Failed to read line");
+    std::io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
     let guess: u32 = guess.trim().parse().expect("Please type a number!");
     guess
 }
 
-
 // Create a function to check the guess
 pub fn check_guess(guess: u32, random_number: u32) -> bool {
-    if guess == random_number {
-        true
-    } else {
-        false
-    }
+    guess == random_number
 }
--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,8 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to the guessing game!");
+    //Please guess a number
+    println!("Please guess a number between 1 and 100");
     // Create a random number
     let random_number = guessing_game::create_random_number();
-    println!("Random number is: {}", random_number);
 
     // Get number from user
     let guess = guessing_game::get_number_from_user();
