a2a67875a14b9fd51645db75d752519cf48508b0
Mehmet Akif Yegin <94207328+makifyegin@users.noreply.github.com>
2024-05-19 21:19:14 +0000

    dev

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
     println!("Hello, world!");
+    // Create a random number
+    let random_number = guessing_game::create_random_number();
+    println!("Random number is: {}", random_number);
+
+    // Get number from user
+    let guess = guessing_game::get_number_from_user();
+    println!("Your guess is: {}", guess);
+
+    // Check the guess
+    let result = guessing_game::check_guess(guess, random_number);
+    if result {
+        println!("You win!");
+    } else {
+        println!("You lose!");
+    }
 }
